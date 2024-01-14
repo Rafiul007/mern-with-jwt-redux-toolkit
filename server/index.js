bb64a7861cd6b9ab4343316fb5ee14cecece865c
Rafie <rafiul.faisal@gmail.com>
2024-01-14 14:49:12 +0600

    server created

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
 app.get('/', (req, res) => {
     console.log("Someone is trying to access the home page")
     res.send(`Hello World! I am running on http://localhost:${port}`)
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server started at ${port}`)
 })
